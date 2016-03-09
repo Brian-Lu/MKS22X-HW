33810b24b4917eebc3581ba0a7456d6c69780681
Brian-Lu <brianlu22@yahoo.com>
2016-03-08 23:54:29 -0500

    Fixed Sorts

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -14,25 +14,23 @@ public class Sorts{
 	    return data;
 	}
 	int[] aryA = new int[(data.length / 2)];
+	int[] aryB;
 	for(int i = 0; i < (data.length / 2); i++) {
 	    aryA[i] = data[i];
 	}
-	int[] aryB;
 	if(data.length % 2 == 1) {
 	    aryB = new int[(data.length / 2) + 1];
 	}
 	else{
 	    aryB = new int[data.length / 2];
 	}
-	for(int i = (data.length / 2); i < data.length; i++) {
-	    aryB[i-(data.length / 2)] = data[i];
+	for(int i = 0; i < data.length - (data.length / 2); i++) {
+	    aryB[i] = data[i + data.length];
 	}
 	return merge(mergesortH(aryA), mergesortH(aryB));
     }
     public static int[] merge(int[] aryA, int[] aryB) {
 	int counter1, counter2;
-	counter1 = 0;
-	counter2 = 0;
 	int[] ret = new int[aryA.length + aryB.length];
 	while(counter1 + counter2 < ret.length) {
 	    if(aryA.length <= counter1) {
