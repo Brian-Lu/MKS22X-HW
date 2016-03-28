9d7fb8e082b9e023e2b10a31f27d80c0d932cd8a
Brian-Lu <brianlu22@yahoo.com>
2016-03-28 01:03:51 -0400

    MyLinkedListSumL

--- 08MyLinkedList/Driver.java
+++ 08MyLinkedList/Driver.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Driver{
     public static void main(String[]args){
         MyLinkedList<String> m = new MyLinkedList<String>();
@@ -13,18 +14,21 @@ public class Driver{
             m.add(-1,"oops");
             System.out.println("\n\nAdd -1 #####################################");
         }catch(IndexOutOfBoundsException e){
+            System.out.println("text");
 
         }
         try{
             m.add(12,"oops");
             System.out.println("\n\n add 12 #####################################");
         }catch(IndexOutOfBoundsException e){
+            System.out.println("text");
 
         }
         try{
             m.remove(12);
             System.out.println("\n\n remove 12 #####################################");
         }catch(IndexOutOfBoundsException e){
+            System.out.println("text");
 
         }
 
@@ -32,6 +36,7 @@ public class Driver{
             m.set(12,"Fwazzat?!?");
             System.out.println("\n\n set 12 #####################################");
         }catch(IndexOutOfBoundsException e){
+            System.out.println("text");
 
         }
 
@@ -40,21 +45,21 @@ public class Driver{
 
 
         //replace toString(true) with a debug to string that shows the head/tail
-        System.out.println(m.toString(true));
+        System.out.println(m.toString());
         System.out.println(n);
 
         //test removing from head/tail/middle
         m.remove(0);
         n.remove(0);
-        System.out.println(m.toString(true));
+        System.out.println(m.toString());
 
         m.remove(2);
         n.remove(2);
-        System.out.println(m.toString(true));
+        System.out.println(m.toString());
 
         m.remove(m.size()-1);
         n.remove(n.size()-1);
-        System.out.println(m.toString(true));
+        System.out.println(m.toString());
         System.out.println(n);
 
         //test adding to end/start
@@ -62,21 +67,19 @@ public class Driver{
         n.add(0,"START");
         m.add(m.size(),"PENULTIMATE");
         n.add(n.size(),"PENULTIMATE");
-        System.out.println(m.toString(true));
+        System.out.println(m.toString());
         System.out.println(n);
 
         //test add
         m.add("Z-END!");
         n.add("Z-END!");
-        System.out.println(m.toString(true));
+        System.out.println(m.toString());
         System.out.println(n);
-
-        //test remove random items:
-        Random rand = new Random(0);
+        Random rand = new Random(2);
         for(int i = 0; i < 6000; i++){
-            int op = rand.nextInt(4);
+            int op = rand.nextInt(3);
 
-            if(op == 0 || n.size()==0){//ensure never empty
+            if(op == 0 || n.size()==0){
                 n.add(""+i);
                 m.add(""+i);
             }else if(op == 1 ){
@@ -91,40 +94,8 @@ public class Driver{
                 }
             }
         }
-        System.out.println(m.toString(true));
+        System.out.println(m);
         System.out.println(n);
 
-        /*test speed of add in front and at end.
-        long start,end;
-        System.out.println("Add 100k to front");
-
-        start = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++)
-            n.add(0,""+i);
-        end = System.currentTimeMillis();
-        System.out.println( "ArrayList "+(end-start)/1000.0 );
-
-        start = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++)
-            m.add(0,""+i);
-        end = System.currentTimeMillis();
-        System.out.println( "LinkedList "+(end-start)/1000.0 );
-
-
-        System.out.println("Add 1m to end");
-
-        start = System.currentTimeMillis();
-        for(int i = 0; i < 1000000; i++)
-            n.add(""+i);
-        end = System.currentTimeMillis();
-        System.out.println( "ArrayList "+(end-start)/1000.0 );
-
-        start = System.currentTimeMillis();
-        for(int i = 0; i < 1000000; i++)
-            m.add(""+i);
-        end = System.currentTimeMillis();
-        System.out.println( "LinkedList "+(end-start)/1000.0 );
-
-        */
     }
 }
--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,144 +1,174 @@
-public class MyLinkedList <T>{
-    private class LNode {
-	public LNode next;
-	public T value;
-	public LNode(T data) {
-	    value = data;
-	}
-	public T getValue() {
-	    return value;
-	}
-	public LNode getNext() {
-	    return next;
-	}
-	public void setValue(T data) {
-	    value = data;
-	}
-	public void setNext(LNode data) {
-	    next = data;
-	}
-    }
+import java.util.*;
+public class MyLinkedList <T> implements Iterable<T>{
     private LNode head;
     private LNode last;
     private int size;
+    private class LNode {
+        public LNode next;
+        public T value;
+        public LNode(T data) {
+            value = data;
+        }
+        public T getValue() {
+            return value;
+        }
+        public LNode getNext() {
+            return next;
+        }
+        public void setValue(T data) {
+            value = data;
+        }
+        public void setNext(LNode data) {
+            next = data;
+        }
+    }
     public int size() {
-	return size;
+        return size;
     }
     public String toString(){
-	String ans = "[";
-	LNode p = head;
-	while(p != null){
-	    ans += p.getValue();
-	    if(p.getNext()!= null){
-		ans+=", ";
-	    }
-	    p = p.getNext();
-	}
-	return ans+"]";
+        String ans = "[";
+        LNode p = head;
+        while(p != null){
+            ans += p.getValue();
+            if(p.getNext()!= null){
+                ans+=", ";
+            }
+            p = p.getNext();
+        }
+        return ans+ "]";
     }
     public T get(int index) {
-	if(index < 0 || index >= size()) {
-	    throw new IndexOutOfBoundsException();
-	}
-	LNode current = head;
-	int i = 0;
-	while(i < index) {
-	    current = current.getNext();
-	    i++;
-	}
-	return current.getValue();
+        if(index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        LNode current = head;
+        int i = 0;
+        while(i < index) {
+            current = current.getNext();
+            i++;
+        }
+        return current.getValue();
     }
     public T set(int index,T newValue) {
-	if(index < 0 || index >= size()) {
-	    throw new IndexOutOfBoundsException();
-	}
-	LNode current = head;
-	int i = 0;
-	while(i < index) {
-	    current = current.getNext();
-	    i++;
-	}
-	T ret = current.getValue();
-	current.setValue(newValue);
-	return ret;
+        if(index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        LNode current = head;
+        int i = 0;
+        while(i < index) {
+            current = current.getNext();
+            i++;
+        }
+        T ret = current.getValue();
+        current.setValue(newValue);
+        return ret;
     }
-    
     public T remove(int index) {
-	if (index >= size || index < 0) {
-	    throw new IndexOutOfBoundsException();
-	}
-	T ret;
-	if (index == 0) {
-	    ret = head.getValue();
-	    head = head.getNext();
-	} else {
-	    LNode current = head;
-	    int i = 0;
-	    while (i + 1 < index) {
-		current = current.getNext();
-		i++;
-	    }
-	    if (index == size - 1) {
-		last = current;
-	    }
-	    ret = current.getNext().getValue();
-	    current.setNext(current.getNext().getNext());
-	}
-	size--;
-	return ret;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        T ret;
+        if (index == 0) {
+            ret = head.getValue();
+            head = head.getNext();
+        } else {
+            LNode current = head;
+            int i = 0;
+            while (i + 1 < index) {
+                current = current.getNext();
+                i++;
+            }
+            if (index + 1== size) {
+                last = current;
+            }
+            ret = current.getNext().getValue();
+            current.setNext(current.getNext().getNext());
+        }
+        size--;
+        return ret;
     }
-
     public boolean add(int index, T value) {
-	if(index < 0 || index > size) {
-	    throw new IndexOutOfBoundsException();
-	}
-	LNode temp = new LNode(value);
-	if(index == 0 || head == null) {
-	    temp.setNext(head);
-	    head = temp;
-	}
-	if(index == size()) {
-	    return add(value);
-	}
-	else{
-	    LNode current = head;
-	    int i = 0;
-	    while(i + 1 < index) {
-		current = current.getNext();
-		i++;
-	    }
-	    temp.setNext(current.getNext());
-	    current.setNext(temp);
-	}
-	size += 1;
-	return true;
+        if(index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        LNode temp = new LNode(value);
+        if(index == 0) {
+            temp.setNext(head);
+            head = temp;
+            size++;
+            return true;
+        }
+        LNode current = head;
+        int i = 0;
+        while(i + 1 < index) {
+            current = current.getNext();
+            i++;
+        }
+        temp.setNext(current.getNext());
+        current.setNext(temp);
+        size++;
+        return true;
     }
     public boolean add(T value) {
-	if(head == null) {
-	    head = new LNode(value);
-	    last = head;
-	}
-	else {
-	    last.setNext(new LNode(value));
-	    last = last.getNext();
-	}
-	size += 1;
-	return true;
+        if(head == null) {
+            head = new LNode(value);
+            last = head;
+        }
+        else {
+            last.setNext(new LNode(value));
+            last = last.getNext();
+        }
+        size += 1;
+        return true;
     }
     public int indexOf(T value) {
-	LNode current = head;
-	int i = 0;
-	while(current != null) {
-	    if(current.getValue().equals(value)) {
-		return i;
-	    }
-	    current = current.getNext();
-	    i++;
-	}
-	return -1;
-    } 
+        LNode current = head;
+        int i = 0;
+        while(current != null) {
+            if(current.getValue().equals(value)) {
+                return i;
+            }
+            current = current.getNext();
+            i++;
+        }
+        return -1;
+    }
+    public Iterator<T> iterator() {
+        return new IteratorH();
+    }
+    private class IteratorH implements Iterator<T> {
+        LNode next;
+        public IteratorH() {
+            next = head;
+        }
+        public boolean hasNext() {
+            if(next == null) {
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        public T next() {
+            if(!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T value = next.getValue();
+            next = next.getNext();
+            return value;
+        }
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+    public static double sumL(MyLinkedList<Double> data) {
+        Double x;
+        x = 0.0;
+        Iterator<Double> a = data.iterator();
+        while(a.hasNext()) {
+            x += a.next();
+        }
+        return x.doubleValue();
+    }
 }
-  
-	    
 
-	    
