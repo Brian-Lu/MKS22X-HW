dfabfd2befb649c2e5bc1754ba28273d28d4ef7e
Brian-Lu <brianlu22@yahoo.com>
2016-03-29 21:48:52 -0400

    STAX BOIIIII

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,47 +1,50 @@
 public class ParenDemo{
     public static void main(String[]args){
-	String input = "()()(([[]]))";
-	if(args.length > 0){
-	    input = args[0];
-	    System.out.println(isMatching(input)); 
-	}else{
-	    System.out.println(isMatching(input));
-	    System.out.println("Usage:"); 
-	    System.out.println("java ParenDemo \"text\""); 
-	}
- 
+        String input = "()()(([[]]))";
+        if(args.length > 0){
+            input = args[0];
+            System.out.println(isMatching(input));
+        }else{
+            System.out.println(isMatching(input));
+            System.out.println("Usage:");
+            System.out.println("java ParenDemo \"text\"");
+        }
+
     }
     public static boolean isMatching(String s) {
-	MyStack<String> stack = new MyStack<String>();
-	for(int i = 0; i < s.length(); i++) {
-	    if(s.substring(i).equals("[") || s.substring(i).equals("(") || s.substring(i).equals("{")) {
-		stack.push(s.substring(i));
-	    }
-	    if(s.substring(i).equals("}")) {
-		if(!(stack.peek().equals("{"))){
-		    return false;
-		}
-		else{
-		    stack.pop();
-		}
-	    }
-	    if(s.substring(i).equals("]")) {
-		if(!(stack.peek().equals("["))){
-		    return false;
-		}
-		else{
-		    stack.pop();
-		}
-	    }
-	    if(s.substring(i).equals(")")) {
-		if(!(stack.peek().equals("("))){
-		    return false;
-		}
-		else{
-		    stack.pop();
-		}
-	    }
-	}
-	return (stack.size() == 0);
+        MyStack<Character> stack = new MyStack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if(a == '{' || a == '[' || a == '(') {
+                stack.push(a);
+            }
+            if(a == '}') {
+                if(stack.isEmpty() || (stack.peek() != '{')){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+            if(a == ']') {
+                if(stack.isEmpty() || (stack.peek() != '[')){
+                        return false;
+                    }
+                    else{
+                        stack.pop();
+                    }
+            }
+            if(a == ')') {
+                if(stack.isEmpty() || (stack.peek() != '(')){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+        }
     }
 }
+
+
+
