0646a487d15054b949574091266ad95e6681f470
Brian-Lu <brianlu22@yahoo.com>
2016-04-05 00:03:47 -0400

    MyDeque

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class MyDeque<T>{
+    private T[] data;
+    private int size;
+    private int start;
+    private int end;
+    public MyDeque() {
+	data = (T[]) new Object[10];
+	size = 0;
+	start = 0;
+	end = 0;
+    }
+    private void grow() {
+    }
+    public void addFirst(T value) {
+	if(size == data.length) {
+	    grow();
+	}
+	if(start >= 1) {
+	    start --;
+	}
+        else if(start == 0) {
+	    start = data.length - 1;
+	}
+	size++;
+	data[start] = value;
+    }
+    public void addLast(T value) {
+	if(size == data.length) {
+	    grow();
+	}
+	if(end < data.length - 1) {
+	    end++;
+	}
+	else if(end == data.length - 1) {
+	    end = 0;
+	}
+	size++;
+	data[end] = value;
+    }
+    public T removeFirst() {
+	if(size < 1) {
+	    throw new NoSuchElementException();
+	}
+	if(start < data.length - 1) {
+	    start++;
+	    size--;
+	    return data[start - 1];
+	}
+	else{ 
+	    start = 0;
+	    size--;
+	    return data[data.length - 1];
+	}
+    }
+    public T removeLast() {
+	if(size < 1) {
+	    throw new NoSuchElementException();
+	}
+	if(end > 0) {
+	    end--;
+	    size--;
+	    return data[end + 1];
+	}
+	else{
+	    end = data.length - 1;
+	    size--;
+	    return data[0];
+	}
+    }
+    public T getFirst() {
+	if(size < 1) {
+	    throw new NoSuchElementException();
+	}
+	return data[start];
+    }
+    public T getLast() {
+	if(size < 1) {
+	    throw new NoSuchElementException();
+	}
+	return data[end];
+    }
+}
+    
